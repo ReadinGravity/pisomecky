892e8c0b28850e4f3cb5a021e439ac58c42ac656
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2022-10-19 08:39:35 +0200

    uloha 2 B

---
+++ fontanka.py
@@ -0,0 +1,6 @@
+import math
+r=float(input("Enter r of a cylinder: "))
+h=float(input("Enter the Height of a cylinde"))
+volume=math.pi*pow(r,2)*h
+print("surface area of a cylinder wll be %.2f" %s_area)
+print("volume of a cylinder will be  %.2f" %volume)
