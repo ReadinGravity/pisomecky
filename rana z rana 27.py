b5bf9ce12d131acc9c29fab06f5262602fee522b
ReadinGravity <113469951+ReadinGravity@users.noreply.github.com>
2024-03-27 08:30:28 +0100

    Update rana z rana 27.py

--- rana z rana 27.py
+++ rana z rana 27.py
@@ -1,36 +1,45 @@
 import tkinter as tk
 
-def kukni_zatvorky (vyraz:str)->bool:
-     zatvorkovac = 0
+def kukni_zatvorky(vyraz: str)->bool:
+    bracket_counter=0
     for i in vyraz:
-        if i == '(':
-            zatvorkovac+=1
-        elif i == ')':
-            zatvorkovac-=1
-            if zatvorkovac < 0:
+        if i=="(":
+            bracket_counter+=1
+        elif i==")":
+            bracket_counter-=1
+            if bracket_counter<0:
                 return False
-    return True if bracket_counter == 0 else False
+    return True if bracket_counter==0 else False
 
-def create_vyraz (vyraz: str)
+def create_vyraz(vyraz: str):
     global colors
-    lave_zatvorky= []
-    sirka_pismena = 20
-    y = sirka_pismena//2
-    color_num = 0
-
-for i in range(len(vyraz)):
-    x=i*sirka_pismena
-    pismeno=vyraz[i]
-    temp=canvas.create_text(x+10,y,text=pismeno,font=f"Arial {sirka_pismena}", anchor="nw")
-        if letter == '(':
-            left_brackets.append(temp)
+    lave_zatvorky=[]
+    pis_sirka=20
+    y=pis_sirka//2
+    color_num=0
+    for i in range(len(vyraz)):
+        x=i*pis_sirka
+        letter=vyraz[i]
+        temp=canvas.create_text(x + 10, y, text=letter, font=f"Helvetica {pis_sirka}", anchor="nw")
+        if letter=='(':
+            lave_zatvorky.append(temp)
             canvas.itemconfig(temp, fill=colors[color_num])
-        elif letter == ')':
-            color = canvas.itemcget(left_brackets[-1], 'fill')
+            color_num+=1
+        elif letter==')':
+            color=canvas.itemcget(lave_zatvorky[-1], "fill")
             canvas.itemconfig(temp, fill=color)
-            left_brackets.pop()
+            lave_zatvorky.pop()
 
 root=tk.Tk()
+
 canvas=tk.Canvas(root, width=600, height=100, bg="white")
 canvas.pack()
 
+colors=["red","orange","cyan","green","blue","purple","pink"]
+vyraz="(2x+y)-x+5(x(x+8))+y"
+
+check=kukni_zatvorky(vyraz)
+if check:
+    create_vyraz(vyraz)
+
+root.mainloop()
